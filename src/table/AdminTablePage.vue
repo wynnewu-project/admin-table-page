f74ff0ceadeb1ae379cf0ce0cbde892da9169458
wynne <wynnewu@live.cn>
2025-07-25 17:27:51 +0800

    feat: add tsconfig and type definitions

--- src/table/AdminTablePage.vue
+++ src/table/AdminTablePage.vue
@@ -1,308 +1,377 @@
-<template> 
-  <el-config-provider :locale="configLocale">
-  <slot name="search">
-    <atp-search
-      v-if="filterFields.length || hiddenFilterFields.length"
-      :showFields="filterFields"
-      :hiddenFields="hiddenFilterFields"
-    />
-  </slot>
-  <el-card> 
-    <slot name="tools">
-      <atp-tool
-        :buttons="toolButtons"
-        :refresh="refresh"
-        @auto-refresh="handleAutoRefresh"
-        @manual-refresh="getTableData"
-        @pause-auto-refresh="handlePauseAutoRefresh"
-      />
-    </slot>
-    <slot name="tips">
-      <el-alert 
-        v-if="tips" 
-        :title="tips" 
-        type="success" 
-        :closable="false"
-        style="margin-bottom: 8px"
-      />
-    </slot>
-    <el-table
-      ref="tableRef"
-      v-loading="loading"
-      :highlight-current-row="selectable === 'single'"
-      :row-key="rowKey"
-      table-layout="auto"
-      @current-change="handleSelectionChange"
-      @selection-change="handleSelectionChange"
-      :header-cell-style="{
-        backgroundColor: '#fafafa'
-      }"
-      border
-      :data=tableData
-      v-bind="$attrs"
-    > 
-      <el-table-column type="index" width="50" v-if="showIndex"/>
-      <el-table-column type="selection" width="50" reserve-selection v-if="selectable === 'multiple'"/>
-      <template 
-        v-for="{ hidden, ...elCol} in tableColumns"
-        :key="elCol.prop"
-      >
-        <el-table-column
-          v-if="!hidden"
-          align="center"
-          v-bind="elCol"
-        > 
-          <template #header> 
-            <slot :name="`${elCol.prop}_header`">{{ elCol.label }}</slot>
-          </template>
-          <template #default="{ row, column }"> 
-            <slot :name="elCol.prop" :row="row" >{{ row[column.property ] }}</slot>
-          </template>
-        </el-table-column>
-      </template>
-      <slot name="extra_columns"/>
-      <el-table-column 
-        v-if="$slots.actions || actionColumn.length"
-        prop="actions"
-        align="center"
-        v-bind="actionColumnProps"
-      > 
-        <template #header>
-          <slot name="actions_header">{{actionColumnLabel ?? translate("label.actionColumn") }}</slot>
-        </template>
-        <template #default="{ row }">
-          <slot name="actions" :row="row">
-            <template 
-              v-for="({text, show=true, onClick}, index) in actionColumn"
-              :key="text"
-            > 
-              <template v-if="show">
-                <el-divider direction="vertical" v-if="index" class="hidden-xs-only"/>
-                <el-button 
-                  type="primary" 
-                  link 
-                  size="small"
-                  @click="() => onClick(row)"
-                >{{text}}</el-button>
-              </template>
-            </template>
-          </slot>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="table-pagination">
-      <el-pagination
-        v-model:current-page="page"
-        v-model:page-size="limit"
-        layout="total, sizes, prev, pager, next, jumper"
-        @size-change="getTableData"
-        @current-change="getTableData"
-        :total="total"
-        v-bind="elPaginationProps"
-      />
-    </div>
-  </el-card>
-  </el-config-provider>
+<template>
+	<el-config-provider :locale="configLocale">
+		<slot name="search">
+			<atp-search
+				v-if="filterFields.length || hiddenFilterFields.length"
+				:showFields="filterFields"
+				:hiddenFields="hiddenFilterFields"
+			/>
+		</slot>
+		<el-card>
+			<slot name="tools">
+				<atp-tool
+					:buttons="toolButtons"
+					:refresh="refresh"
+					@auto-refresh="handleAutoRefresh"
+					@manual-refresh="getTableData"
+					@pause-auto-refresh="handlePauseAutoRefresh"
+				/>
+			</slot>
+			<slot name="tips">
+				<el-alert
+					v-if="tips"
+					:title="tips"
+					type="success"
+					:closable="false"
+					style="margin-bottom: 8px"
+				/>
+			</slot>
+			<el-table
+				ref="tableRef"
+				v-loading="loading"
+				:highlight-current-row="selectable === 'single'"
+				:row-key="rowKey"
+				table-layout="auto"
+				@current-change="handleSelectionChange"
+				@selection-change="handleSelectionChange"
+				:header-cell-style="{
+					backgroundColor: '#fafafa',
+				}"
+				border
+				:data="tableData"
+				v-bind="$attrs"
+			>
+				<el-table-column
+					type="index"
+					width="50"
+					v-if="showIndex"
+				/>
+				<el-table-column
+					type="selection"
+					width="50"
+					reserve-selection
+					v-if="selectable === 'multiple'"
+				/>
+				<template
+					v-for="{ hidden, ...elCol } in tableColumns"
+					:key="elCol.prop"
+				>
+					<el-table-column
+						v-if="!hidden"
+						align="center"
+						v-bind="elCol"
+					>
+						<template #header>
+							<slot :name="`${elCol.prop}_header`">{{ elCol.label }}</slot>
+						</template>
+						<template #default="{ row, column }">
+							<slot
+								:name="elCol.prop"
+								:row="row"
+								>{{ row[column.property] }}</slot
+							>
+						</template>
+					</el-table-column>
+				</template>
+				<slot name="extra_columns" />
+				<el-table-column
+					v-if="$slots.actions || actionColumn.length"
+					prop="actions"
+					align="center"
+					v-bind="actionColumnProps"
+				>
+					<template #header>
+						<slot name="actions_header">{{
+							actionColumnLabel ?? translate("label.actionColumn")
+						}}</slot>
+					</template>
+					<template #default="{ row }">
+						<slot
+							name="actions"
+							:row="row"
+						>
+							<template
+								v-for="({ text, show = true, onClick }, index) in actionColumn"
+								:key="text"
+							>
+								<template v-if="show">
+									<el-divider
+										direction="vertical"
+										v-if="index"
+										class="hidden-xs-only"
+									/>
+									<el-button
+										type="primary"
+										link
+										size="small"
+										@click="() => onClick(row)"
+										>{{ text }}</el-button
+									>
+								</template>
+							</template>
+						</slot>
+					</template>
+				</el-table-column>
+			</el-table>
+			<div class="table-pagination">
+				<el-pagination
+					v-if="paginationable"
+					v-model:current-page="page"
+					v-model:page-size="limit"
+					layout="total, sizes, prev, pager, next, jumper"
+					@size-change="getTableData"
+					@current-change="getTableData"
+					:total="total"
+					v-bind="elPaginationProps"
+				/>
+			</div>
+		</el-card>
+	</el-config-provider>
 </template>
 
-<script> 
+<script
+	setup
+	lang="ts"
+	generic="
+		DataType extends Record<string, unknown>,
+		TotalKey extends string = 'total',
+		ItemsKey extends string = 'items'
+	"
+>
 //import { ElTable, ElPagination, ElCard, ElTableColumn, ElButton, ElDivider } from "element-plus";
 import AtpTool from "../table-tool/ATPTool.vue";
 import AtpSearch from "../table-search/ATPSearch.vue";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import en from "element-plus/es/locale/lang/en";
 
-import { computed, onMounted, onUnmounted, provide, ref, watch } from "vue";
+import {
+	computed,
+	onMounted,
+	onUnmounted,
+	provide,
+	ref,
+	toValue,
+	useTemplateRef,
+	watch,
+} from "vue";
 import tableProps from "./props";
 import { EL_TABLE_METHOD } from "./elTableMethod";
 import useMediaQuery from "../utils/useMediaQuery";
-import { filter, isNull, isString, omitBy, slice, isArray } from "lodash/fp";
+import {
+	filter,
+	isNull,
+	isString,
+	omitBy,
+	slice,
+	isArray,
+	prop,
+} from "lodash/fp";
 import { useLocale } from "../locals/useLocale";
 import { debounce } from "lodash";
 import { createI18n } from "vue-i18n";
+import type {
+	PaginatedResult,
+	SearchField,
+	TableColumn,
+	TableProps,
+} from "@/type/table";
+import { pa } from "element-plus/es/locales.mjs";
 
+const props = withDefaults(
+	defineProps<TableProps<DataType, TotalKey, ItemsKey>>(),
+	{
+		locale: "zhCn",
+		showIndex: false,
+		searchAreaMode: "card",
+		localData: () => [],
+		toolButtons: () => [],
+		actionColumn: () => [],
+		totalKey: () => "total" as TotalKey,
+		itemsKey: () => "items" as ItemsKey,
+		paginationable: true,
+		rowKey: "id",
+	},
+);
 
+const emits = defineEmits<{
+	(selectRows: DataType | DataType[]): void;
+}>();
 
-export default {
-  props: tableProps,
-  emits: ["selectChange"],
-  components: {
-    AtpTool,
-    AtpSearch
-  },
-  data() {
-    return {
-      tableRef: null,
-      tableData: [],
-      filteredData: this.localData,
-      timer: null,
-      loading: false,
-      selectedRows: [],
-      page: 1,
-      limit: 10,
-      total: this.localData.length ?? 0,
-      query: {},
-      queryChange: false,
-      filterFields: [],
-      hiddenFilterFields: [],
-      tableColumns: [],
-    }
-  },
-  setup() {
-    const { breakpoint } = useMediaQuery();
-    const { translate, setLocale } = useLocale();
-    return { breakpoint, translate, setLocale };
-  },
-  computed: {
-    configLocale() {
-      return this.locale === "zhCn" ? zhCn : en;
-    },
-    offset() {
-      return (this.page - 1) * this.limit;
-    },
-  },
-  created() {
-    this.columns.forEach(({ prop, label,  filterable, filterOptions={}}) => {
-      if(!(filterable || JSON.stringify(filterOptions) !== "{}")) 
-        return;
-      const { defaultHidden=false, defaultValue=null } = filterOptions;
-      this.query[prop] = filterOptions?.defaultValue ?? null;
-      const field = {
-        name: prop,
-        label,
-        ...filterOptions
-      }
+const tableRef = useTemplateRef("tableRef");
+const tableData = ref<DataType[]>([]);
+const filteredData = ref<DataType[]>(props.localData);
+const timer = ref<number | null>(null);
+const loading = ref(false);
+const selectedRows = ref<DataType[]>([]);
+const page = ref(1);
+const pageSize = ref(10);
+const total = ref(props.localData.length ?? 0);
+const query = ref<Record<string, any>>({});
+const queryChange = ref(false);
+const searchFields = ref<SearchField[]>([]);
+const hiddenSearchFields = ref<SearchField[]>([]);
+const tableColumns = ref<TableColumn[]>();
 
-      defaultHidden ? this.hiddenFilterFields.push(field) : this.filterFields.push(field);
-    })
-    this.tableColumns = this.columns.map(c => {
-      if(typeof c === "string") {
-        return {
-          prop: c,
-          label: c[0].toUpperCase() + c.slice(1),
-          hidden: false
-        }
-      }
-      return c;
-    })
-  },
-  provide() {
-    return {
-      breakpoint: computed(() => this.breakpoint),
-      query: this.query,
-      translate: this.translate
-    }
-  },
-  expose: ["reload", "getSelections"],
-  methods: {
-    async fetchRemoteData(params) {
-      const finalQuery = {
-        ...this.query,
-        ...this.extraQuery,
-        ...params,
-        page: this.page,
-        offset: this.offset,
-        limit: this.limit
-      }
-      const res = await this.fetchMethod(finalQuery);
-      this.total = res[this.totalKey] ?? 0;
-      this.tableData = res[this.listKey] ?? [];
-    },
+const { breakpoint } = useMediaQuery();
+const { translate, setLocale } = useLocale();
 
-    filterLocalData(params) {
-      if(this.queryChange || params ) {
-        const filterParams = omitBy((value) => {
-          return isNull(value) || value === "" || !value.length 
-        })({
-          ...this.query,
-          ...this.extraQuery,
-          ...params
-        });
-        this.filteredData = filter((o) => {
-          let filter = true;
-          for(let [key, value] of Object.entries(filterParams)) {
-            if(isString(value)) {
-              const testStr = new RegExp(value+'');
-              filter = testStr.test(o[key]);
-            } else if(isArray(value)){
-              filter = value.includes(o[key]);
-            } else {
-              filter = value === o[key];
-            }
-            if(!filter) break;
-          }
-          return filter
-        })(this.localData);
-      }
-      this.tableData = slice(
-        this.offset,
-        this.offset + this.limit
-      )(this.filteredData);
-    },
+const configLocale = computed(() => {
+	return props.locale === "zhCn" ? zhCn : en;
+});
 
-    getTableData(params) {
-      this.loading = true;
-      if(this.localData && !this.fetchMethod) {
-        this.filterLocalData(params);
-      } else {
-        this.fetchRemoteData(params);
-      }
-      this.loading = false;
-    },
-    handleQueryChange() {
-      const func = debounce(this.getTableData, 500);
-      func();
-    },
-    handleAutoRefresh() {
-      this.timer = setInterval(this.getTableData, this.refresh);
-    },
-    handlePauseAutoRefresh() {
-      clearInterval(this.timer);
-    },
-    handleSelectionChange(selectRows) {
-      if(this.selectable !== "multiple" || Array.isArray(selectRows)){
-        this.selectedRows = selectRows ?? [];
-        this.$emit("selectChange", this.selectedRows);
-      }
-    },
-    handleClearSelection() {
-      this.tableRef.clearSelection();
-    },
-    getSelections() { return this.selectedRows },
-    reload(params) {
-      this.getTableData(params);
-    }
-  },
-  mounted() {
-    this.getTableData();
-    if(typeof(this.refresh) === 'number') {
-      this.handleAutoRefresh();
-    }
-    const tableRefObj = this.$refs['tableRef'];
-    for(let k of Object.keys(tableRefObj)) {
-      if(typeof tableRefObj[k] === "function") {
-        this[k] = tableRefObj[k];
-      }
-    }
+const formatColumns = () => {
+	const formatted = props.columns.map((col) => {
+		if (typeof col === "string") {
+			return {
+				prop: col,
+				label: col[0].toUpperCase() + col.slice(1),
+				hidden: false,
+			};
+		}
+		return col;
+	});
+	formatted.forEach((col) => {
+		const { prop, label, searchable, searchConfig } = col;
+		if (
+			!searchable &&
+			(searchConfig === undefined || JSON.stringify(searchConfig) === "{}")
+		) {
+			return;
+		}
+		if (!prop) {
+			return;
+		}
+		const {
+			defaultHidden = false,
+			defaultValue = undefined,
+			type = "input",
+		} = searchConfig ?? {};
+		query.value[prop] = defaultValue;
 
-  }, 
-  unmounted() {
-    this.handlePauseAutoRefresh();
-  },
-  watch: {
-    query: {
-      async handler() {
-        this.queryChange = true;
-        this.handleQueryChange();
-      },
-      deep: true
-    },
-    locale(lang) {
-      this.setLocale(lang);
-    }
-  }
-}
+		const field: SearchField = {
+			name: prop,
+			label: label ?? prop[0].toUpperCase() + prop.slice(1),
+			type,
+			...searchConfig,
+		};
 
+		defaultHidden
+			? hiddenSearchFields.value.push(field)
+			: searchFields.value.push(field);
+	});
+};
+
+formatColumns();
+
+provide("breakpoint", breakpoint);
+provide("query", query);
+provide("translate", translate);
+
+const fetchRemoteData = async (params: Record<string, unknown> = {}) => {
+	const finalQuery = {
+		...query.value,
+		...toValue(props.extraQuery ?? {}),
+		...params,
+		page: page.value,
+		pageSize: pageSize.value,
+		offset: (page.value - 1) * pageSize.value,
+		limit: pageSize.value,
+	};
+	const res = await props.fetchMethod!(finalQuery);
+	if (props.paginationable) {
+		total.value =
+			(res as PaginatedResult<DataType, TotalKey, ItemsKey>)[
+				props.totalKey as TotalKey
+			] ?? 0;
+		tableData.value =
+			(res as PaginatedResult<DataType, TotalKey, ItemsKey>)[
+				props.itemsKey as ItemsKey
+			] ?? [];
+		return;
+	}
+	tableData.value = res as DataType[];
+};
+
+const filterLocalData = (params: Record<string, unknown> = {}) => {
+	if (queryChange || (params && JSON.stringify(params) !== "{}")) {
+		const filterParams = omitBy((value) => {
+			return (
+				value === null ||
+				value === "" ||
+				value === undefined ||
+				(Array.isArray(value) && !value.length)
+			);
+		})({
+			...query.value,
+			...toValue(props.extraQuery),
+			...params,
+		});
+		filteredData.value = props.localData.filter((data) => {
+			let filter = true;
+			for (let [key, value] of Object.entries(filterParams)) {
+				const rowValue = data[key];
+				if (typeof value === "string") {
+					const testStr = new RegExp(value);
+					filter = testStr.test(data[key] as string);
+				} else if (Array.isArray(value)) {
+					filter = value.includes(data[key]);
+				} else {
+					filter = value === rowValue;
+				}
+				if (!filter) {
+					break;
+				}
+			}
+			return filter;
+		});
+	}
+	tableData.value = filteredData.value.slice(
+		(page.value - 1) * pageSize.value,
+		page.value * pageSize.value,
+	);
+};
+
+const getTableData = async (params: Record<string, unknown> = {}) => {
+	try {
+		loading.value = true;
+		if (props.localData && !props.fetchMethod) {
+			filterLocalData(params);
+		} else {
+			await fetchRemoteData(params);
+		}
+	} finally {
+		loading.value = false;
+	}
+};
+
+const handleQueryChange = debounce(getTableData, 300);
+const handleAutoRefresh = () => {
+	if (timer.value) {
+		window.clearInterval(timer.value);
+	}
+	timer.value = window.setInterval(
+		getTableData,
+		(props.refresh as number) ?? 5000,
+	);
+};
+
+const handlePauseAutoRefresh = () => {
+	if (timer.value) {
+		window.clearInterval(timer.value);
+		timer.value = null;
+	}
+};
+
+onMounted(() => {
+	getTableData();
+	if (typeof props.refresh === "number") {
+		handleAutoRefresh();
+	}
+});
+
+onUnmounted(() => {
+	handlePauseAutoRefresh();
+});
 </script>
 
 <style lang="scss">
---
+++ src/type/table.ts
@@ -0,0 +1,197 @@
+import type {
+	ButtonProps as ElButton,
+	TableColumnCtx as ElTableColumn,
+	PaginationProps as ElPaginationProps,
+} from "element-plus";
+import type { Component, Ref } from "vue";
+
+/**
+ * render component type for the search field
+ */
+type SearchRenderType = "input" | "select" | "date";
+
+interface ColumnSearchConfig {
+	/**
+	 * The type of the search component.
+	 */
+	type: SearchRenderType;
+	/**
+	 * The placeholder text for the search column.
+	 */
+	placeholder?: string;
+	/**
+	 * Whether the search component for the column is hidden by default.
+	 */
+	defaultHidden?: boolean;
+	/**
+	 * The options for the select component when `type` is `select`.
+	 */
+	selectOptions?: Array<{ label: string; value: unknown }> | unknown[];
+	/**
+	 * default value for the search component.
+	 */
+	defaultValue?: unknown;
+}
+
+export interface SearchField extends ColumnSearchConfig {
+	name: string;
+	label: string;
+}
+
+export interface TableColumn<T = unknown> extends Partial<ElTableColumn<T>> {
+	/**
+	 * Hidden the column when `true`.
+	 */
+	hidden?: boolean;
+	/**
+	 * The column can be searched when `searchable` is true.
+	 */
+	searchable?: boolean;
+	/**
+	 * The search component configuration for the column.
+	 */
+	searchConfig?: ColumnSearchConfig;
+}
+
+interface ToolButton extends ElButton {
+	/**
+	 * The label for the tool button.
+	 */
+	label: string;
+	/**
+	 *  A function that is called when the button is clicked.
+	 */
+	onClick: () => void;
+	/**
+	 * Hides the button when `true`.
+	 */
+	hidden?: boolean | Ref<boolean>;
+}
+
+interface RowAction<DataType> extends ElButton {
+	/**
+	 * The label for the action button.
+	 */
+	label?: string;
+	/**
+	 * The icon for the action button.
+	 */
+	icon?: string | Component;
+	/**
+	 * The function that is called when the action button is clicked.
+	 */
+	onClick: (row: unknown) => void;
+	/**
+	 * Whether the action button is hidden
+	 */
+	hidden?: boolean | Ref<boolean> | ((row: DataType) => boolean);
+}
+
+export type PaginatedResult<
+	DataType,
+	TotalKey extends string = "total",
+	ItemsKey extends string = "items",
+> = {
+	[K in TotalKey]: number;
+} & {
+	[K in ItemsKey]: DataType[];
+};
+
+export interface TableProps<
+	DataType,
+	TotalKey extends string = "total",
+	ItemsKey extends string = "items",
+> {
+	/**
+	 * columns of the table
+	 */
+	columns: (TableColumn | string)[];
+	/**
+	 * The component used to Wrap the table. Default is `el-card`.
+	 */
+	tableWrapper?: Component;
+	/**
+	 * The mode of the search area.
+	 */
+	searchAreaMode?: "card" | "inline";
+	/**
+	 * Locale for the table
+	 */
+	locale?: "zhCn" | "en";
+	/**
+	 * Whether to show the index column
+	 */
+	showIndex?: boolean;
+	/**
+	 * Local data for the table
+	 * If `fetchMethod` is provided, this will be ignored.
+	 */
+	localData?: DataType[];
+	/**
+	 * Whether the table is paginated.
+	 */
+	paginationable?: boolean;
+	/**
+	 * The method to fetch data for the table.
+	 */
+	fetchMethod?: (
+		query: Record<string, unknown>,
+	) =>
+		| Promise<PaginatedResult<DataType, TotalKey, ItemsKey>>
+		| Promise<DataType[]>;
+	/**
+	 * How to refresh the table data.
+	 * - `manual`: A refresh button will be shown to manually refresh the data.
+	 * - `number`: The data will be fetched every `number` milliseconds automatically.
+	 * - `undefined`: refresh is disabled.
+	 */
+	refresh?: "manual" | number;
+	/**
+	 * A list of tool buttons to be displayed above the table.
+	 */
+	toolButtons?: ToolButton[];
+	/**
+	 * How to select rows in the table.
+	 * - `single`: Only one row can be selected at a time.
+	 * - `multiple`: Multiple rows can be selected at a time, support selecting rows on different pages.
+	 * - `undefined`: No selection is allowed.
+	 */
+	selectable?: "single" | "multiple";
+	/**
+	 * - The key used to identify each row in the table.
+	 * - It's required when `selectable` is not `undefined`.
+	 */
+	rowKey?: string;
+	/**
+	 * The operations column configuration.
+	 */
+	actionColumn?: RowAction<DataType>[];
+	/**
+	 * The label for the action column. When locale is `zhCn`, the default is `操作`, otherwise it's `Operations`.
+	 */
+	actionColumnLabel?: string;
+	/**
+	 * Props for the action column.
+	 */
+	actionColumnProps?: TableColumn;
+	/**
+	 * The key used to get the total number of items in the paginated result.
+	 */
+	totalKey?: TotalKey;
+	/**
+	 * The key used to get the items in the paginated result.
+	 */
+	itemsKey?: ItemsKey;
+	/**
+	 * Extra query parameters to be sent for fetching data.
+	 */
+	extraQuery?: Record<string, unknown>;
+	/**
+	 * Props for the el-pagination.
+	 */
+	elPaginationProps?: ElPaginationProps;
+	/**
+	 * A tip for the table, displayed at the top of the table, under the tool buttons.
+	 */
+	tips?: string;
+}
